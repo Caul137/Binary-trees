f96bf6808e60c0d070ad9887b2e8690ee8ece69d
caul137 <salomao.caua@gmail.com>
2026-03-31 18:22:03 -0300

    new

---
+++ Bst-js.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+    let current = this.root;
+
+    while (true) {
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
+    
+  }
+
+}
+
+const tree = new BinaryTree()
+
+tree.insert(10)
+tree.insert(5)
+tree.insert(20)
+tree.insert(3)
+tree.insert(9)
+tree.insert(7)
+
+console.log(tree.root)
